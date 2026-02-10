ea1bd0234770e78337e4660b25f7707184260d4b
IaroslavKr <iaroslavkr1104@gmail.com>
2026-02-10 15:44:09 +0900

    ver 1.0

--- src/main/ru/practicum/moviehub/api/ErrorResponse.java
+++ src/main/ru/practicum/moviehub/api/ErrorResponse.java
@@ -3,35 +3,29 @@ package ru.practicum.moviehub.api;
 import java.util.List;
 
 public class ErrorResponse {
-
     private final int status;
     private final String error;
     private final List<String> details;
 
-    public ErrorResponse(int status, String error)
-    {
+    public ErrorResponse(int status, String error) {
         this(status, error, null);
     }
 
-    public ErrorResponse(int status, String error, List<String> details)
-    {
+    public ErrorResponse(int status, String error, List<String> details) {
         this.status = status;
         this.error = error;
         this.details = details;
     }
 
-    public int getStatus()
-    {
+    public int getStatus() {
         return status;
     }
 
-    public String getError()
-    {
+    public String getError() {
         return error;
     }
 
-    public List<String> getDetails()
-    {
+    public List<String> getDetails() {
         return details;
     }
 }
